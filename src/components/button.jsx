1e5aec424dec3462c3e743c7f2b2760a9cbed741
Evasive <albinshiju285@gmail.com>
2025-03-10 09:37:24 +0530

    Update button.jsx

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+
+const ViewProductButton = ({ onClick }) => {
+  return (
+    <button
+      className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+      onClick={onClick}
+    >
+      View Product
+    </button>
+  );
+};
+
+export default ViewProductButton;
 // write button card here
